1dd62bd639ef09f6b23916ca50389f024c597266
Jean Chung <jxchung@gmail.com>
2017-03-18 16:47:50 -0500

    starting to lay out homepage content

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,17 +7,17 @@ class Home extends Component {
         <div className="hero-img">
           <img src="http://www.arshtcenter.org/Global/PressRoom/photos/hi/Spring%20Awakening%20photo%20by%20Paul%20Kolnick.jpg" alt="A scene from Spring Awakening" height="100%" width="100%" />
         </div>
-        
-        <h1>Hero Img</h1>
-
-        <div className="clearfix">
-          <div className="col col-2">
-            <h1>About</h1>
-            <h1>News Feed</h1>
+        <div className="content">
+          <div className="content-left">
+            <p>Chicago D(ART), or Deaf ART, is the creative home for artists looking to bridge the communication barrier between the d/Deaf and hearing worlds.</p>
           </div>
-          <div className="col col-2">
-            <h1>Call to Action</h1>
-            <h1>Calender</h1>
+          <div className="right-sidebar">
+            <div>
+              <h3>Call to Action</h3>
+            </div>
+            <div>
+              <h3>Calendar (Agenda)</h3>
+            </div>
           </div>
         </div>
       </div>
