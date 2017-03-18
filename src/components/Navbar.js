95bcf924f6d2849dba03bcb3d53c01983eff5854
Jean Chung <jxchung@gmail.com>
2017-03-18 14:09:45 -0500

    Installed router package

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import logo from '../../public/images/logo.svg';
 
 class Navbar extends Component {
-  render(){
-    <div></div>
+  render() {
+    <div />;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-//components
+// components
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Sidebar from './components/Sidebar';
@@ -20,16 +20,16 @@ import Auditions from './components/Auditions';
 
 ReactDOM.render(
   <Router>
-    <Navbar/>
-      <div>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/events" component={Events} />
-        <Route exact path="/donate" component={Donate} />
-        <Route exact path="/auditions" component={Auditions} />
-        <Route exact path="/accessibility" component={Accessibility} />
-      </div>
-    <Footer/>
+    <div>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/events" component={Events} />
+      <Route exact path="/donate" component={Donate} />
+      <Route exact path="/auditions" component={Auditions} />
+      <Route exact path="/accessibility" component={Accessibility} />
+      <Footer />
+    </div>
   </Router>,
   document.getElementById('root')
 );
