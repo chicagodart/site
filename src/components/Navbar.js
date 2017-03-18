51c2f791bc884f7591bc8c00def49805f5ca58a0
Jean Chung <jxchung@gmail.com>
2017-03-18 14:22:13 -0500

    Added headings to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Nav</h1>
-      </div>
-    );
-  }
-}
+const Navbar = props => (
+  <nav className="navbar">
+    <div className="navbar-brand">
+      <Link to="/">Chicago D(ART)</Link>
+    </div>
+    <div className="navbar-menu-right">
+      <ul>
+        <li><Link to="/">About</Link></li>
+        <li><Link to="/">Tickets & Events</Link></li>
+        <li><Link to="/">Donations</Link></li>
+        <li><Link to="/">Auditions</Link></li>
+        <li><Link to="/">Accessibility</Link></li>
+      </ul>
+    </div>
+  </nav>
+);
 
 export default Navbar;
