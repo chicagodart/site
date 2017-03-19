f6b3abf12fb778fd25934c23686ff3cb34440259
Jean Chung <jxchung@gmail.com>
2017-03-18 19:33:21 -0500

    Nav menu is now centered

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import {connect, Provider} from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { connect, Provider } from 'react-redux';
 import App from './App';
-import './index.css';
+// import './index.css';
 import axios from 'axios';
 import store from './store';
 
@@ -23,7 +23,7 @@ import Auditions from './components/Auditions';
 //   <Provider store={store}>
 //     <Router>
 //       <Route path='/' component={App} />
-        
+
 //     </Router>
 // </Provider>,
 
