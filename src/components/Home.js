ea9e03487969b78885fbde92df7b5b9b31ae873a
Hal Carleton <hello@halleycarleton.com>
2017-03-19 04:48:40 -0500

    Basic news feed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { loadPosts } from '../reducers/posts';
 
 class Home extends Component {
+  componentDidMount() {
+    this.props.loadPosts('uncategorized');
+  }
+
   render() {
+    const posts = Object.keys(this.props.posts).map(key => this.props.posts[key]).sort((a, b) => {
+      const dateA = new Date(a.date_gmt);
+      const dateB = new Date(b.date_gmt);
+      if (dateA < dateB) {
+        return -1;
+      }
+      if (dateA > dateB) {
+        return 1;
+      }
+      return 0;
+    });
+
     return (
       <div>
         <div className="hero-img">
@@ -14,6 +33,11 @@ class Home extends Component {
                 <p>Chicago D(ART), or Deaf ART, is the creative home for artists looking to bridge the communication barrier between the d/Deaf and hearing worlds.</p>
               </div>
               <h2>News</h2>
+              <ol className="event-list">
+                {posts.map(post =>
+                  <PostInList key={post.id} post={post} />
+                )}
+              </ol>
             </div>
             <div className="col col-4 center">
               <h2>Call to Action</h2>
@@ -28,4 +52,23 @@ class Home extends Component {
   }
 }
 
-export default Home;
+function PostInList(props) {
+  const post = props.post;
+  return (
+    <li className="event-in-list">
+      <Link to={`/events/${post.slug}`}>
+        <div className="event-in-list__display-img">
+          <img src="http://lorempixel.com/400/300" alt="" />
+        </div>
+        <h3 className="event-in-list__title">{post.title.rendered}</h3>
+        <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+      </Link>
+    </li>
+  );
+}
+
+const mapStateToProps = ({ posts }) => ({ posts });
+
+const mapDispatchtoProps = { loadPosts };
+
+export default connect(mapStateToProps, mapDispatchtoProps)(Home);
