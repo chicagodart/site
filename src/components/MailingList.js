5d3cdffd3a03a3e3ab58eb00a4fcc45cba9efa7a
Jean Chung <jxchung@gmail.com>
2017-03-19 02:34:40 -0500

    added styling to signup form

--- src/components/MailingList.js
+++ src/components/MailingList.js
@@ -11,7 +11,7 @@ class MailingList extends Component {
         <div className="hero-img">
           <img src="http://www.arshtcenter.org/Global/PressRoom/photos/hi/Spring%20Awakening%20photo%20by%20Paul%20Kolnick.jpg" alt="A scene from Spring Awakening" height="100%" width="100%" />
         </div>
-        <div id="mc_embed_signup clearfix mx3">
+        <div className="clearfix mx3" id="mc_embed_signup">
           <form
             action="//chicagodart.us14.list-manage.com/subscribe/post?u=9a004eb3afa0b524d3daa367b&amp;id=93ba1f45a0"
             method="post"
@@ -42,7 +42,11 @@ class MailingList extends Component {
               <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true">
                 <input type="text" name="b_9a004eb3afa0b524d3daa367b_93ba1f45a0" tabIndex="-1" value="" />
               </div>
-              <div className="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" />
+              <div className="mc-field-group col sm-col-6">
+                <label />
+                <button type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe">
+                  <span id="mailing-list-submit">Submit</span>
+                </button>
               </div>
             </div>
           </form>
