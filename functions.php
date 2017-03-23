bca6606469ca5c71a23c7a04f502e8eabc1c1f49
Hal Carleton <hello@halleycarleton.com>
2017-03-23 18:55:33 -0500

    Fixing index.php

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+function include_react_files() {
+  wp_enqueue_style( 'main-styles', get_template_directory_uri() . '/build/static/css/main.f07f54cf.css', array(), null);
+  wp_enqueue_script( 'main-scripts', get_template_directory_uri() . '/build/static/js/main.c5b92b13.js'), array(), null);
+}
+
+add_action( 'wp_enqueue_scripts', 'include_react_files' );
+?>
--- index.php
+++ index.php
@@ -1,18 +1,10 @@
-<php?
+<?php
+/*
+Plugin Name: dart-site
+Description: Plugin created from react-create-app.
+Version:     0.0.1
+Author:      Heap O Nerds
+*/
 
-# Plugin Name: dart-site
-# Description: Plugin created from react-create-app.
-# Version:     0.0.1
-# Author:      Heap O Nerds
-
-#function wp_dart_site() {
-  #echo '<div id="root"></div>';
-#}
-
-function include_react_files() {
-    wp_enqueue_style( 'main-styles', './build/static/css/');
-    wp_enqueue_script( 'main-scripts', './build/static/js/');
-}
-
-add_action( 'wp_enqueue_scripts', 'include_react_files' );
-require(dirname(__FILE__) . '/build/index.html'); ?>
+require(dirname(__FILE__) . '/build/index.html');
+?>
