ced517fd71cc873ffc60c9c4a8a40cdbc792aeb1
Hal Carleton <hello@halleycarleton.com>
2017-04-03 16:16:19 -0500

    Adding call to action

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { loadPosts } from '../reducers/posts';
 
 import HeroImage from './HeroImage';
+import Sidebar from './Sidebar';
 
 class Home extends Component {
   componentDidMount() {
@@ -30,32 +31,30 @@ class Home extends Component {
         <HeroImage src={page ? page.acf.hero_image.sizes.medium_large : ' '} alt={page ? page.acf.hero_image.title : ' '} />
 
         <div className="max-width-12">
-          <div className="clearfix content-sidebar-container">
-            <div className="col col-8">
-              <div id="lil-about" dangerouslySetInnerHTML={{ __html: page ? page.content.rendered : '' }} />
-              <h2>News</h2>
-              <ol className="event-list">
-                {posts.map(post =>
-                  <PostInList key={post.id} post={post} />
-                )}
-              </ol>
+          <div className="clearfix content-excerpt-section">
+            <div className="sm-col sm-col-6 px3">
+              <div dangerouslySetInnerHTML={{ __html: page ? page.content.rendered : '' }} />
             </div>
-            <div className="col col-4 center">
-              {/* TODO: Homepage sidebar
-                <h2>Call to Action</h2>
-                <div id="mini-cal">
-                  <h2>Calendar</h2>
-                </div>*/}
+            <div className="sm-col sm-col-6 px3">
+              <p>
+                {!!page && page.acf.call_to_action}
+              </p>
             </div>
           </div>
         </div>
+        <h2>News</h2>
+        <ol className="event-list">
+          {!!posts && posts.map(post =>
+            <PostInList key={!!post && post.id} post={post} />
+          )}
+        </ol>
       </div>
     );
   }
 }
 
 function PostInList(props) {
-  const post = props.post;
+  const { post } = props;
   const img = post.content.rendered.slice(0, post.content.rendered.indexOf('</') + 4);
   const content = post.content.rendered.slice(post.content.rendered.indexOf('</') + 4);
   return (
