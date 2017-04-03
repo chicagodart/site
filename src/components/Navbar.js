420b69f302aecd881a8b6ce4551997a05b51ad0d
Jean Chung <jxchung@gmail.com>
2017-04-03 12:01:51 -0500

    responsive navbar menu button is aligned in Chrome as well as Firefox

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -45,35 +45,37 @@ class Navbar extends React.Component {
     return (
       <div className="clearfix">
         <nav className="navbar col col-12">
-          <div className="navbar-brand">
-            <Link to="/">
-              <div>Chicago</div>
-              <div><span>D</span><span id="art-parens">(ART)</span></div>
-            </Link>
-          </div>
-          <nav className="navbar-menu-center">
-            <i className="fa fa-bars toggle-nav fa-lg" onClick={this.toggleMenu} aria-hidden="true" />
-            <ul id={this.showHideMenu()}>
-              <li onClick={this.closeMenu}><Link to="/about">About</Link></li>
-              <li onClick={this.closeMenu}><Link to="/events">Tickets & Events</Link></li>
-              <li onClick={this.closeMenu}><Link to="/donate">Donate</Link></li>
-              <li onClick={this.closeMenu}><Link to="/auditions">Auditions</Link></li>
-              <li onClick={this.closeMenu}><Link to="/accessibility">Accessibility</Link></li>
-            </ul>
-          </nav>
-          <div className="navbar-menu-right">
-            <ul>
-              <li onClick={toggleContrast}>
-                <a href="#">
-                  <i className="fa fa-adjust fa-lg" title="Toggle light/dark view" />
-                </a>
-              </li>
-              <li onClick={toggleFontSize}>
-                <a href="#">
-                  <i className="fa fa-font fa-lg" title="Toggle to large font view" />
-                </a>
-              </li>
-            </ul>
+          <div className="navbar-container">
+            <div className="navbar-brand">
+              <Link to="/">
+                <div>Chicago</div>
+                <div><span>D</span><span id="art-parens">(ART)</span></div>
+              </Link>
+            </div>
+            <nav className="navbar-menu-center">
+              <ul id={this.showHideMenu()}>
+                <li onClick={this.closeMenu}><Link to="/about">About</Link></li>
+                <li onClick={this.closeMenu}><Link to="/events">Tickets & Events</Link></li>
+                <li onClick={this.closeMenu}><Link to="/donate">Donate</Link></li>
+                <li onClick={this.closeMenu}><Link to="/auditions">Auditions</Link></li>
+                <li onClick={this.closeMenu}><Link to="/accessibility">Accessibility</Link></li>
+              </ul>
+            </nav>
+            <div className="navbar-menu-right">
+              <ul>
+                <li onClick={toggleContrast}>
+                  <a href="#">
+                    <i className="fa fa-adjust fa-lg" title="Toggle light/dark view" />
+                  </a>
+                </li>
+                <li onClick={toggleFontSize}>
+                  <a href="#">
+                    <i className="fa fa-font fa-lg" title="Toggle to large font view" />
+                  </a>
+                </li>
+                <i className="fa fa-bars fa-lg toggle-nav" onClick={this.toggleMenu} aria-hidden="true" />
+              </ul>
+            </div>
           </div>
         </nav>
       </div>
